49d49b15978b835ec687a1f7294d91d05003810a
eduatigs <eduatigs96@gmail.com>
2020-12-20 16:40:41 -0500

    algoritmos de realce

--- ROI_personas.py
+++ ROI_personas.py
@@ -2,6 +2,39 @@ from cv2 import cv2
 import numpy as np
 from glob import glob
 
+# Applying filters
+def filtroUnsharping(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray,(5,5),0)
+    img_fil = blur / 150
+    img_res = img - img_fil
+    img_al = cv2.equalizeHist(img)
+    return img_al
+
+def filtroHomomorfico(img):
+    img = np.float32(img)
+    img = img/255
+
+    rows,cols,dim=img.shape
+    rh, rl, cutoff = 2.5,0.5,32
+    imgYCrCb = cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
+    y,cr,cb = cv2.split(imgYCrCb)
+
+    y_log = np.log(y+0.01)
+    y_fft = np.fft.fft2(y_log)
+    y_fft_shift = np.fft.fftshift(y_fft)
+
+    DX = cols/cutoff
+    G = np.ones((rows,cols))
+    for i in range(rows):
+        for j in range(cols):
+            G[i][j]=((rh-rl)*(1-np.exp(-((i-rows/2)**2+(j-cols/2)**2)/(2*DX**2))))+rl
+
+    result_filter = G * y_fft_shift
+    result_interm = np.real(np.fft.ifft2(np.fft.ifftshift(result_filter)))
+    result = np.exp(result_interm)
+    return result
+
 # Load Yolo
 net = cv2.dnn.readNetFromDarknet("yolov3.cfg","yolov3.weights")
 classes = []
