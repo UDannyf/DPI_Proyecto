3d07adb5de51c87b981a8594692c16bc2c09b85f
eduatigs <eduatigs96@gmail.com>
2020-12-21 12:32:19 -0500

    filtros changes

--- ROI_personas.py
+++ ROI_personas.py
@@ -88,35 +88,17 @@ def segmentacionWarershed(img):
     return img
 
 def filtroUnsharping(img):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray,(5,5),0)
-    img_fil = blur / 150
-    img_res = img - img_fil
-    img_al = cv2.equalizeHist(img_res)
+    gaussian_3 = cv2.GaussianBlur(img, (0, 0), 20)
+    img_al = cv2.addWeighted(img, 1.5, gaussian_3, -0.5, 0)
     return img_al
 
-def filtroHomomorfico(img):
-    img = np.float32(img)
-    img = img/255
+def filtroSharpen(img):
+    sharpen = np.array((
+    [0, -1, 0],
+    [-1, 5, -1],
+    [0, -1, 0]), dtype="int")
 
-    rows,cols,dim=img.shape
-    rh, rl, cutoff = 2.5,0.5,32
-    imgYCrCb = cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
-    y,cr,cb = cv2.split(imgYCrCb)
-
-    y_log = np.log(y+0.01)
-    y_fft = np.fft.fft2(y_log)
-    y_fft_shift = np.fft.fftshift(y_fft)
-
-    DX = cols/cutoff
-    G = np.ones((rows,cols))
-    for i in range(rows):
-        for j in range(cols):
-            G[i][j]=((rh-rl)*(1-np.exp(-((i-rows/2)**2+(j-cols/2)**2)/(2*DX**2))))+rl
-
-    result_filter = G * y_fft_shift
-    result_interm = np.real(np.fft.ifft2(np.fft.ifftshift(result_filter)))
-    result = np.exp(result_interm)
+    result = cv2.filter2D(img, -1, sharpen)
     return result
 
 def yoloImage(imagen):
@@ -206,9 +188,9 @@ while(True):
         cv2.waitKey(0)
         
     elif (op=='3'):
-        print ("\n\tRealzado filtroHomomorfico\n")
+        print ("\n\tRealzado filtro Sharpen\n")
         img = cv2.imread(path,1)
-        r1 = filtroHomomorfico(img)
+        r1 = filtroSharpen(img)
         clon = yoloImage(r1)
         cv2.imshow("Imagen", r1)
         cv2.imshow("Imagen Yolo", clon)
