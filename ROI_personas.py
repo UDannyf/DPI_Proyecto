4c305da0990b6e57bf93691282629ac735a7c1fc
UDannyf <danny_u89@hotmail.com>
2021-01-20 11:56:52 -0500

    final

--- ROI_personas.py
+++ ROI_personas.py
@@ -1,38 +1,22 @@
+import os
+import sys
 from cv2 import cv2
 from glob import glob
 import numpy as np
 from matplotlib import pyplot as plt
 
-def segmentacionColor(img):
+def s1(img): #segmentacionColor
     image = img
-    num_clusters = 10
+    num_clusters = 100
     # Creamos una copia para poderla manipular a nuestro antojo.
     image_copy = np.copy(image)
- 
-    # Mostramos la imagen y esperamos que el usuario presione cualquier tecla para continuar.
-    #cv2.imshow('Imagen', image)
-    #cv2.waitKey(0)
- 
-    # Convertiremos la imagen en un arreglo de ternas, las cuales representan el valor de cada pixel. En pocas palabras,
-    # estamos aplanando la imagen, volviéndola un vector de puntos en un espacio 3D.
     pixel_values = image_copy.reshape((-1, 3))
     pixel_values = np.float32(pixel_values)
-    
-    # Abajo estamos aplicando K-Means. Como siempre, OpenCV es un poco complicado en su sintaxis, así que vamos por partes.
-    
-    # Definimos el criterio de terminación del algoritmo. En este caso, terminaremos cuando la última actualización de los
-    # centroides sea menor a *epsilon* (cv2.TERM_CRITERIA_EPS), donde epsilon es 1.0 (último elemento de la tupla), o bien
-    # cuando se hayan completado 10 iteraciones (segundo elemento de la tupla, criterio cv2.TERM_CRITERIA_MAX_ITER).
     stop_criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-    
-    # Este es el número de veces que se correrá K-Means con diferentes inicializaciones. La función retornará los mejores
-    # resultados.
     number_of_attempts = 10
     
-    # Esta es la estrategia para inicializar los centroides. En este caso, optamos por inicialización aleatoria.
     centroid_initialization_strategy = cv2.KMEANS_RANDOM_CENTERS
-    
-    # Ejecutamos K-Means con los siguientes parámetros:
+        # Ejecutamos K-Means con los siguientes parámetros:
     # - El arreglo de pixeles.
     # - K o el número de clusters a hallar.
     # - None indicando que no pasaremos un arreglo opcional de las mejores etiquetas.
@@ -55,14 +39,10 @@ def segmentacionColor(img):
     segmented_data = centers[labels.flatten()]
     
     # Debemos reestructurar el arreglo de datos segmentados con las dimensiones de la imagen original.
-    segmented_image = segmented_data.reshape(image_copy.shape)
-    
-    # Mostramos la imagen segmentada resultante.
-    #cv2.imshow('Imagen segmentada', segmented_image)
-    #cv2.waitKey(0)
+    segmented_image = segmented_data.reshape(image_copy.shape)    
     return segmented_image
 
-def segmentacionWarershed(img):
+def s2(img): #segmentacionWarershed
     img = img
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
@@ -87,12 +67,12 @@ def segmentacionWarershed(img):
     img[markers == -1] = [255,0,0]
     return img
 
-def filtroUnsharping(img):
+def r1(img): #filtroUnsharping
     gaussian_3 = cv2.GaussianBlur(img, (0, 0), 20)
     img_al = cv2.addWeighted(img, 1.5, gaussian_3, -0.5, 0)
     return img_al
 
-def filtroSharpen(img):
+def r2(img): #filtroSharpen
     sharpen = np.array((
     [0, -1, 0],
     [-1, 5, -1],
@@ -101,7 +81,21 @@ def filtroSharpen(img):
     result = cv2.filter2D(img, -1, sharpen)
     return result
 
-def yoloImage(imagen):
+def detectFace(image): 
+    faceClassif = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = faceClassif.detectMultiScale(gray,
+    scaleFactor=1.1,
+    minNeighbors=5)
+    countFace = 0
+    for (x,y,w,h) in faces:
+        cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
+        countFace = countFace +1
+    #print("conteo de rostros: ",countFace)
+    return image, countFace
+    
+def yolo(imagen):
     net = cv2.dnn.readNetFromDarknet("yolov3.cfg","yolov3.weights")
     classes = []
     with open("coco.names", "r") as f:
@@ -155,66 +149,121 @@ def yoloImage(imagen):
                 cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
                 cv2.putText(img, label, (x, y + 30), font, 1, color, 1)
                 count=count+1
-    print('conteo de Personas: ',count)
-    return img
+    #print('conteo de Personas: ',count)
+    return img, count
 
-################### munu############
+################### Combinacion segmentacion-realzado #################
 
-#path = "images/img1.jpg"
-#path = "images/img2.jpg"
-#path = "images/img3.jpg"
-path = "images/img4.jpg"
-#path = "images/img5.jpg"
-while(True):
-    print ("\n\n\t0. Salir\n\t1. Cargar una imagen\n\t2. Realzado filtro Unsharping"+
-           "\n\t3. Realzado filtro Sharpen \n\t4. Segmentacion Warershed  \n\t5. Segmentacion Color\n")
-    op = input("\n\tIngrese la opcion --> ")
+def s1r1(image):    
+    img1 = s1(image)
+    img2 = r1(img1)
+    img, count = yolo(img2)
+    img, face = detectFace(img)    
+    return img, count, face
 
-    if (op=='0'):
+def s1r2(image):
+    img1 = s1(image)
+    img2 = r2(img1)
+    img, count = yolo(img2)
+    img, face = detectFace(img)    
+    return img, count, face
+
+def s2r1(image):
+    img1 = s2(image)
+    img2 = r1(img1)
+    img, count = yolo(img2)    
+    img, face = detectFace(img)    
+    return img, count, face
+
+def s2r2(image):
+    img1 = s2(image)
+    img2 = r2(img1)
+    img, count = yolo(img2)    
+    img, face = detectFace(img)    
+    return img, count, face
+
+def mayor(num1, num2, num3, num4):
+    arreglo = np.array([num1, num2, num3, num4])
+    maxElement = np.amin(arreglo)
+    i1 = 0
+    i2 = 0
+    i3 = 0
+    i4 = 0
+    if maxElement == num1:
+        i1 = 1
+    if maxElement == num2:
+        i2 = 1
+    if maxElement == num3:
+        i3 = 1
+    if maxElement == num4:
+        i4 = 1
+    return i1, i2, i3, i4
+    
+            
+
+
+plistx = ["seg1_real_1","seg1_real2","seg2_real1","seg2_real2"]
+plisty = [0,0,0,0]
+rlistx = ["seg1_real_1","seg1_real2","seg2_real1","seg2_real2"]
+rlisty = [0,0,0,0]
+
+for path in glob('img_prueba/*.jpg'):
+    os.system("cls")
+    img = cv2.imread(path, 1)    
+    cv2.imshow("Imagen", img)
+    cv2.waitKey(0)
+    pr0 = input("\n\tIngrese # personas --> ")
+    fc0 = input("\n\tIngrese # rotros --> ")
+    p0 = int(pr0)
+    f0 = int(fc0)
+    if (p0 == -1):
         break
-    elif (op=='1'):
-        print ("\n\tCargar una imagen desde un archivo\n")
-        img = cv2.imread(path, 1)
-        clon = yoloImage(img)
-        cv2.imshow("Imagen", img)
-        cv2.imshow("Imagen Yolo", clon)
-        cv2.waitKey(0)
-        
-    elif (op=='2'):
-        print ("\n\t Realzado filtro Unsharping\n")
-        img = cv2.imread(path,1)
-        r1 = filtroUnsharping(img)
-        clon = yoloImage(r1)
-        cv2.imshow("Imagen", r1)
-        cv2.imshow("Imagen Yolo", clon)
-        cv2.waitKey(0)
-        
-    elif (op=='3'):
-        print ("\n\tRealzado filtro Sharpen\n")
-        img = cv2.imread(path,1)
-        r1 = filtroSharpen(img)
-        clon = yoloImage(r1)
-        cv2.imshow("Imagen", r1)
-        cv2.imshow("Imagen Yolo", clon)
-        cv2.waitKey(0)
-    
-    elif (op=='4'):
-        print ("\n\tSegmentacion Warershed\n")
-        img = cv2.imread(path,1)
-        s1 = segmentacionWarershed(img)
-        clon = yoloImage(s1)
-        cv2.imshow("Imagen Segmentada", s1)
-        cv2.imshow("Imagen Yolo", clon)
-        cv2.waitKey(0)
-    
-    elif (op=='5'):
-        print ("\n\tSegmentación Color\n")
-        img = cv2.imread(path,1)
-        s2 = segmentacionColor(img)
-        clon = yoloImage(s2)
-        cv2.imshow("Imagen Segmentada", s2)
-        cv2.imshow("Imagen Yolo", clon)
-        cv2.waitKey(0)
-    else:
-        print ("\n\tLa opción no es válida.")     
+    img1, p1, f1 = s1r1(img)
+    img2, p2, f2 = s1r2(img)
+    img3, p3, f3 = s2r1(img)
+    img4, p4, f4 = s2r2(img)
+    ep1 = 100 -(p1*100/p0)
+    ep2 = 100 -(p2*100/p0)
+    ep3 = 100 -(p3*100/p0)
+    ep4 = 100 -(p4*100/p0)
+    er1 = 100 -(f1*100/f0)
+    er2 = 100 -(f2*100/f0)
+    er3 = 100 -(f3*100/f0)
+    er4 = 100 -(f4*100/f0)
+    i0,i1,i2,i3 = mayor(ep1,ep2,ep3,ep4)
+    x0,x1,x2,x3 = mayor(er1,er2,er3,er4)
+    plisty[0] +=i0
+    plisty[1] +=i1
+    plisty[2] +=i2
+    plisty[3] +=i3
+    rlisty[0] +=x0
+    rlisty[1] +=x1
+    rlisty[2] +=x2
+    rlisty[3] +=x3
+    #cv2.imshow('Segmentación1_realzado1', img1)
+    #cv2.imshow('Segmentación1_realzado2', img1)
+    #cv2.imshow('Segmentación2_realzado1', img1)
+    #cv2.imshow('Segmentación2_realzado2', img1)
+    #concat_horizontal = cv2.hconcat([img1, img2])
+    #concat_horizontal1 = cv2.hconcat([img3, img4])
+    #cv2.imshow('concat_horizontal', concat_horizontal)
+    #cv2.imshow('concat_horizontal', concat_horizontal1)
+    print("Datos      |  GT  | Segmenta1_realza1 | Segmenta1_realza2 | Segmenta2_realza1 | Segmenta2_realza2")
+    print("Personas   |  %d  |      %d           |         %d        |        %d         |         %d       " % (p0, p1, p2, p3, p4))
+    print("Err_Per    | 0.0  |      %.2f         |         %.2f      |        %.2f       |         %.2f       " % (ep1, ep2, ep3, ep4))
+    print("Rostros    |  %d  |      %d           |         %d        |        %d         |         %d       " % (f0, f1, f2, f3, f4))
+    print("Err_rost   | 0.0  |      %.2f         |         %.2f      |        %.2f       |         %.2f       " % (er1, er2, er3, er4))    
+    cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+plt.bar(plistx,plisty,align = "center")
+plt.title("Mejor rendimiento para Detectar personas")
+plt.xlabel("Algoritmos")
+plt.ylabel("Imagenes")
+plt.show()
+
+plt.bar(rlistx, rlisty, align = "center")
+plt.title("Mejor rendimiento para detectar Rostros")
+plt.xlabel("Algoritmos")
+plt.ylabel("Imagenes")
+plt.show()
